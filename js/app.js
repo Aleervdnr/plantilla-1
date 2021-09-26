0d314a41b762c9d3744b92f7914329f1cd371f1f
Aleervdnr <alejonahuelrott@gmail.com>
2021-09-26 14:57:39 -0300

    section galeria

--- js/app.js
+++ js/app.js
@@ -27,3 +27,52 @@ const sticky = () =>{
 }
 
 sticky()
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+const body = document.querySelector("body")
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.classList.add("disabled")
+}
+
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    body.classList.add("disabled")
+  }
+
+  img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    body.classList.add("disabled")
+  }
+
+  img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    body.classList.add("disabled")
+  }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  body.classList.remove("disabled")
+}
